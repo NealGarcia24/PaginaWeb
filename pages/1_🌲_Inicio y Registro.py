b8272b780c666a4d8d3b873ecfb430e4c0f1800d
NealGarcia24 <139014365+NealGarcia24@users.noreply.github.com>
2023-07-16 09:45:32 -0500

    Update 1_🌲_Inicio y Registro.py

--- pages/1_🌲_Inicio y Registro.py
+++ pages/1_🌲_Inicio y Registro.py
@@ -12,7 +12,7 @@ def main():
     if choice == "Inicio":
         st.subheader("Inicio")
         image = "https://i.ibb.co/kHt7wrB/MoodUp.jpg"
-        st.image(image, use_column_width=0.8)
+        st.image(image, width=300)
         st.write("MoodUp es una herramienta diseñada para detectar la depresión temprana en estudiantes de secundaria del Colegio Los                       Pinos. Nuestro objetivo principal es promover la salud mental y brindar apoyo a los alumnos en su bienestar emocional                     de la mano con nuestros psicólogos especialistas")
 
     elif choice == "Registro":
