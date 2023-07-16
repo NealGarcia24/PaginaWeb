e668f6d6cd5c7c06e184170101da8fe908265a73
NealGarcia24 <139014365+NealGarcia24@users.noreply.github.com>
2023-07-16 11:04:34 -0500

    Update 2_🧱_DetectEmotion.py

--- pages/2_🧱_DetectEmotion.py
+++ pages/2_🧱_DetectEmotion.py
@@ -1,78 +1,79 @@
 import cv2
 import streamlit as st
-from tensorflow.keras.models import load_model
+import av
 import numpy as np
+from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
 
+# Carga el modelo pre-entrenado para la detección de rostros y emociones
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+emotion_model = cv2.dnn.readNetFromCaffe(
+    'deploy.prototxt.txt',
+    'fer2013_mini_XCEPTION.102-0.66.hdf5'
+)
 
-# Cargar el modelo de detección de emociones preentrenado
-model = load_model('model_v6_23.hdf5')
+# Lista de etiquetas de emociones
+EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
-# Definir las etiquetas de las emociones
-emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+# Clase para procesar el video y realizar la detección de emociones en cada cuadro
+class EmotionDetector(VideoProcessorBase):
+    def __init__(self):
+        super().__init__()
+    
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        # Convierte el cuadro de video a una imagen en formato OpenCV
+        image = frame.to_ndarray(format="bgr24")
+        
+        # Detecta rostros en la imagen
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+        # Procesa cada rostro detectado
+        for (x, y, w, h) in faces:
+            face = image[y:y+h, x:x+w]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            face = cv2.resize(face, (48, 48))
+            face = face.astype("float") / 255.0
+            face = np.expand_dims(face, axis=0)
+            face = np.expand_dims(face, axis=0)
 
-# Función para preprocesar la imagen
-def preprocess_image(image):
-    # Convertir a escala de grises
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Redimensionar la imagen a 48x48
-    resized = cv2.resize(gray, (48, 48))
-    # Expandir las dimensiones de la imagen para que coincida con el formato de entrada del modelo (1 canal)
-    resized = np.expand_dims(resized, axis=-1)
-    # Normalizar la imagen
-    normalized = resized / 255.0
-    # Devolver la imagen preprocesada
-    return normalized
+            # Realiza la predicción de emociones
+            emotion_model.setInput(face)
+            preds = emotion_model.forward()
+            emotion_index = np.argmax(preds[0])
 
+            # Obtiene la etiqueta de la emoción y la muestra en la imagen
+            emotion_label = EMOTIONS[emotion_index]
+            cv2.putText(
+                image,
+                emotion_label,
+                (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                (0, 0, 255),
+                2,
+                cv2.LINE_AA
+            )
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
 
-# Función para detectar emociones en el rostro
-def detect_emotions(face):
-    # Preprocesar la imagen
-    processed = preprocess_image(face)
-    # Realizar la predicción utilizando el modelo
-    prediction = model.predict(np.expand_dims(processed, axis=0))[0]
-    # Obtener la emoción con mayor probabilidad
-    max_index = np.argmax(prediction)
-    emotion = emotions[max_index]
-    # Devolver la emoción detectada y la probabilidad correspondiente
-    return emotion, prediction[max_index]
+# Configura la interfaz de Streamlit
+def main():
+    st.title("Detección de emociones en tiempo real")
+    st.write("Utilizando Streamlit-WebRTC y OpenCV")
 
+    # Inicia la transmisión de video y procesa los cuadros con la clase EmotionDetector
+    webrtc_ctx = webrtc_streamer(
+        key="emotion-detection",
+        mode=1,
+        video_processor_factory=EmotionDetector,
+    )
 
-# Configurar la aplicación Streamlit
-st.title('Detección de emociones en tiempo real')
+    if webrtc_ctx.video_processor:
+        st.write("**Transmisión de video en tiempo real**")
+        st.write("La detección de emociones se aplicará a los rostros detectados.")
+    else:
+        st.write("Esperando la transmisión de video...")
 
-# Crear el objeto de captura de video
-video_capture = cv2.VideoCapture(1)
-
-# Ejecutar la aplicación en bucle hasta que se interrumpa
-while True:
-    # Leer un fotograma del video
-    ret, frame = video_capture.read()
-
-    # Si no se pudo leer el fotograma, salir del bucle
-    if not ret:
-        break
-
-    # Obtener las dimensiones del fotograma
-    height, width, _ = frame.shape
-
-    # Dibujar un rectángulo en el fotograma para mostrar el área de detección
-    cv2.rectangle(frame, (100, 100), (width - 100, height - 100), (0, 255, 0), 2)
-
-    # Obtener el área de interés (ROI) dentro del rectángulo
-    roi = frame[100:height - 100, 100:width - 100]
-
-    # Si hay una ROI válida
-    if roi.shape[0] > 0 and roi.shape[1] > 0:
-        # Detectar emociones en la ROI
-        emotion, probability = detect_emotions(roi)
-
-        # Escribir la emoción detectada y la probabilidad en el fotograma
-        cv2.putText(frame, f'{emotion} ({probability:.2f})', (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
-                    cv2.LINE_AA)
-
-    # Mostrar el fotograma en la aplicación Streamlit
-    st.image(frame, channels='BGR')
-
-# Liberar el objeto de captura de video
-video_capture.release()
+if __name__ == "__main__":
+    main()
